27900ce14b0f9d7e01f28e107f870b42def92071
Md Faroque <Omarf.android@gmail.com>
2018-06-10 21:01:03 -0500

    list update

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'React';
 import { connect } from 'react-redux';
+import { ListView } from 'react-native';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
+    componentWillMount() {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.dataSource = ds.cloneWithRows(this.props.libraries);
+    }
+    renderRow(library) {
+        return <ListItem library={library} />;
+    }
     render() {
         console.log(this.props);
-        return;
+        return (
+            <ListView
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            />
+        );
     }
 
 }
 
-// It reads state data and pass as a props to component
+// It reads state data and pass as a props to component.
+// in props, this.props.libraries will return all the data
 const mapStateToProps = state => {
     return { libraries: state.libraries };
 };
